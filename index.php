77b617bfbd936eea74a600c8002561000c1e0395
slmnabd <salmanabdurrohman11@gmail.com>
2018-03-23 11:09:18 +0700

    navbar durung nganan

--- index.php
+++ index.php
@@ -5,10 +5,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Mainshop</title>
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="js/bootstrap.js"></script>
+  <link rel="stylesheet" href="css/style.css">
+
 </head>
 <body>
-  <div class="nav">
-KAKAKAKA
+  <div>
     <?php include "layout/header.php"; ?>
   </div>
 
--- layout/header.php
+++ layout/header.php
@@ -1,4 +1,19 @@
-<div class="navbar">
-  <h1>MAINSHOP</h1>
-  <p>njajal header sek</p>
-</div>
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <a class="navbar-brand" href="#">MainShop</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav nganan">
+      <li class="nav-item">
+        <a class="nav-link" href="#">keranjang</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">daftar</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">masuk</a>
+      </li>   
+    </ul>
+  </div>  
+</nav>
