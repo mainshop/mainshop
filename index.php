36ad1a6e4add568200df19d8555a5c9f6ab86d9d
muh saferagic <muh.saferagic@gmail.com>
2018-03-28 22:07:38 +0700

    (no branch): Auto stash before revert of "navbar durung nganan"

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+<<<<<<< Updated upstream
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -23,3 +24,54 @@
 >>>>>>> BALIK RESET KE ffff
 </body>
 </html>
+=======
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>MainShop</title>
+  </head>
+  <body>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <img src="">
+        <div class="float-left">
+          <a class="navbar-brand text-light" href="#">MAIN</a><a class="navbar-brand text-danger" href="">SHOP</a>          
+        </div>        
+        <div class="collapse navbar-collapse">
+            <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="search" placeholder="Cari" aria-label="Search">
+              <button class="btn my-2 my-sm-0 bg-danger" type="submit">Cari</button>
+          </form>               
+        </div>
+        <div class="float-right">
+          <ul class="navbar-nav mr-auto float-right">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Masuk</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <form>
+                      <a href="">MASUK</a>
+                      <input type="email" name="email">
+                      <input type="password" name="password">
+                    </form>
+                  <div class="dropdown-divider"></div>
+                    Belum punya akun ?<a href="">Buat Akun</a>
+                </div>
+              </li>
+          </ul>   
+        </div>
+    </nav>
+    </header>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
+>>>>>>> Stashed changes
