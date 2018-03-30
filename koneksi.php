7e98537e50ade20290575cefaafb8d46958d4edc
slmnabd <salmanabdurrohman11@gmail.com>
2018-03-31 00:04:22 +0700

    nyoh ki

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+  $server = "localhost";
+  $user = "root";
+  $password = "root";
+  $db = "mainshop";
+  $koneksi = mysqli_connect($server,$user,$password,$database) or die("Koneksi ke database gagal");
+?>
